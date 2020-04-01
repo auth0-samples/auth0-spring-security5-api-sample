aca28ae7e34f391505df09219c2bf3e0409ce30f
Jim Anderson <jim.anderson@auth0.com>
2020-04-01 14:14:06 -0500

    Remove lombok and update Spring version (#6)

--- 01-Authorization-MVC/src/main/java/com/auth0/example/model/Message.java
+++ 01-Authorization-MVC/src/main/java/com/auth0/example/model/Message.java
@@ -1,14 +1,17 @@
 package com.auth0.example.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 /**
  * Simple domain object for our API to return a message.
- * Uses Lombok to remove boilerplate code, see https://projectlombok.org/ for more info.
  */
-@AllArgsConstructor
-@Getter
 public class Message {
     private final String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    @SuppressWarnings("unused")
+    public String getMessage() {
+        return this.message;
+    }
 }
