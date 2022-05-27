80e58a0d8c79934a16f42fa3337564fd41374a48
Poovamraj T T <poovamraj@gmail.com>
2022-05-27 13:12:25 +0200

    Remove use of domain objects

--- 01-Authorization-MVC/src/main/java/com/auth0/example/web/APIController.java
+++ 01-Authorization-MVC/src/main/java/com/auth0/example/web/APIController.java
@@ -1,6 +1,5 @@
 package com.auth0.example.web;
 
-import com.auth0.example.model.Message;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class APIController {
 
     @GetMapping(value = "/public")
-    public Message publicEndpoint() {
-        return new Message("All good. You DO NOT need to be authenticated to call /api/public.");
+    public String publicEndpoint() {
+        return "All good. You DO NOT need to be authenticated to call /api/public.";
     }
 
     @GetMapping(value = "/private")
-    public Message privateEndpoint() {
-        return new Message("All good. You can see this because you are Authenticated.");
+    public String privateEndpoint() {
+        return "All good. You can see this because you are Authenticated.";
     }
 
     @GetMapping(value = "/private-scoped")
-    public Message privateScopedEndpoint() {
-        return new Message("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
+    public String privateScopedEndpoint() {
+        return "All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope";
     }
 }
--- 01-Authorization-WebFlux/src/main/java/com/auth0/example/web/APIController.java
+++ 01-Authorization-WebFlux/src/main/java/com/auth0/example/web/APIController.java
@@ -1,6 +1,5 @@
 package com.auth0.example.web;
 
-import com.auth0.example.model.Message;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,17 +18,17 @@ import reactor.core.publisher.Mono;
 public class APIController {
 
     @GetMapping(value = "/public")
-    public Mono<Message> publicEndpoint() {
-        return Mono.just(new Message("All good. You DO NOT need to be authenticated to call /api/public."));
+    public Mono<String> publicEndpoint() {
+        return Mono.just("All good. You DO NOT need to be authenticated to call /api/public.");
     }
 
     @GetMapping(value = "/private")
-    public Mono<Message> privateEndpoint() {
-        return Mono.just(new Message("All good. You can see this because you are Authenticated."));
+    public Mono<String> privateEndpoint() {
+        return Mono.just("All good. You can see this because you are Authenticated.");
     }
 
     @GetMapping(value = "/private-scoped")
-    public Mono<Message> privateScopedEndpoint() {
-        return Mono.just(new Message("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope"));
+    public Mono<String> privateScopedEndpoint() {
+        return Mono.just("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
     }
 }
