64a6cc83458082467b95c6acd44352d3af25788a
Jim Anderson <jimranderson@gmail.com>
2023-02-07 21:11:28 -0600

    update webflux example to Spring Boot 3

--- 01-Authorization-WebFlux/src/main/java/com/auth0/example/security/SecurityConfig.java
+++ 01-Authorization-WebFlux/src/main/java/com/auth0/example/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.auth0.example.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
@@ -13,6 +14,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
  * Configures our application with Spring Security to restrict access to our API endpoints.
  */
 @EnableWebFluxSecurity
+@Configuration
 public class SecurityConfig {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
