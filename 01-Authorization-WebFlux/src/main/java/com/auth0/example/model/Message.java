02a0a5a5dac3d176f802b5bb22c31a2a1248ecef
Jim Anderson <jimranderson@gmail.com>
2023-10-23 19:41:20 -0500

    Update samples to use okta spring boot starter and spring boot 3

--- 01-Authorization-WebFlux/src/main/java/com/auth0/example/model/Message.java
+++ 01-Authorization-WebFlux/src/main/java/com/auth0/example/model/Message.java
@@ -3,15 +3,4 @@ package com.auth0.example.model;
 /**
  * Simple domain object for our API to return a message.
  */
-public class Message {
-    private final String message;
-
-    public Message(String message) {
-        this.message = message;
-    }
-
-    @SuppressWarnings("unused")
-    public String getMessage() {
-        return this.message;
-    }
-}
+public record Message(String message) {}
